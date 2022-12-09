730c118a500d0bea61110802043b3e97f0d5795d
Rob Anderson <rtanderson2@gmail.com>
2022-12-09 08:43:29 -0900

    removed floating point

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1191,10 +1191,10 @@
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 //#define Z_PROBE_FEEDRATE_FAST (4*60)
-#define Z_PROBE_FEEDRATE_FAST (8*60)
+#define Z_PROBE_FEEDRATE_FAST (10*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 1.5)
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
 
 /**
  * Probe Activation Switch
